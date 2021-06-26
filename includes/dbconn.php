0a0257985e30a0d1c9f03b426d4646690fd41206
AnantharoobanBR <71723048+AnantharoobanBR@users.noreply.github.com>
2021-06-26 15:59:37 +0530

    Add files via upload

---
+++ includes/dbconn.php
@@ -0,0 +1,9 @@
+<?php
+    
+    $con = new mysqli('localhost', 'root', '', 'placement_automation');
+    if (!$con) 
+    {
+        die("Connection failed!!!!");
+    }
+
+?>
---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>PLACEMENT AUTOMATION SYSTEM</h4>
+            <ul class="bottom">
+                <li>Helping to smoothen the hiring process!</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>CONTACT THROUGH</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="mailto:rahulananth12@gmail.com">pl.aut.sy@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: (+91) 9600896680</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>ADDRESS</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>Coimbatore, India</li>
+                <li><i class="glyphicon glyphicon-envelope"></i> PIN CODE : 641 004</li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; Anantharooban's Placement Automation System</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
